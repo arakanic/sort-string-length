46a31004fd7fd83b431e2fcb6a62dd866cde5f37
Ara Kanic <ji.arakani@gmail.com>
2022-06-05 08:00:25 -0400

    Update sortstringlength.js

--- sortstringlength.js
+++ sortstringlength.js
@@ -2,4 +2,10 @@
 
 function sortByLength (array) {
     return array.sort((a,b) => a.length - b.length);
-}  
+}
+
+sortByLength(["Beg", "Life", "I", "To"]) // -> ["I", "To", "Beg", "Life"]
+
+sortByLength(["", "Moderately", "Brains", "Pizza"]) // -> ["", "Pizza", "Brains", "Moderately"]
+
+sortByLength(["Longer", "Longest", "Short"]) // -> ["Short", "Longer", "Longest"]
